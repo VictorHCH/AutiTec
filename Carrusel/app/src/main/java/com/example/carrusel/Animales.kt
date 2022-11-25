017202485e9c97c8f63556e9f015e49db8c8056b
Bat3d2 <102492542+Bat3d2@users.noreply.github.com>
2022-11-25 00:01:33 -0700

    Version 3.2.7
    Delay animales

--- Carrusel/app/src/main/java/com/example/carrusel/Animales.kt
+++ Carrusel/app/src/main/java/com/example/carrusel/Animales.kt
@@ -3,7 +3,9 @@ package com.example.carrusel
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ImageButton
+
 
 
 class Animales : AppCompatActivity() {
@@ -22,43 +24,119 @@ class Animales : AppCompatActivity() {
         val decirpato = findViewById<ImageButton>(R.id.patito)
         val decirpavo = findViewById<ImageButton>(R.id.pavito)
 
+        val DURACION: Long = 3000;
+        val CORTA: Long = 1000;
+
         decirmono.setOnClickListener{
+
+            decirmono.isEnabled=false
+
             val mp = MediaPlayer.create(this,R.raw.monosonido)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    decirmono.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
 
         decirpato.setOnClickListener{
+            decirpato.isEnabled=false
+
             val mp = MediaPlayer.create(this,R.raw.patosonido)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    decirpato.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
 
         decirpavo.setOnClickListener{
+            decirpavo.isEnabled=false
+
             val mp = MediaPlayer.create(this,R.raw.pavosonido)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    decirpavo.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
         decirburro.setOnClickListener{
+            decirburro.isEnabled=false
+
             val mp = MediaPlayer.create(this, R.raw.burro)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    decirburro.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
 
         decircaballo.setOnClickListener{
+            decircaballo.isEnabled=false
+
             val mp = MediaPlayer.create(this, R.raw.caballo)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    decircaballo.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
 
         decircabra.setOnClickListener{
+            decircabra.isEnabled=false
+
             val mp = MediaPlayer.create(this, R.raw.cabra)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    decircabra.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
 
         decirgallina.setOnClickListener{
+            decirgallina.isEnabled=false
+
             val mp = MediaPlayer.create(this, R.raw.gallina)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    decirgallina.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
 
         decirgato.setOnClickListener{
+            decirgato.isEnabled=false
+
             val mp = MediaPlayer.create(this, R.raw.gato)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    decirgato.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
     }
 }
