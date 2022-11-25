e793cdcd4b3706c909fe2d548cc1b081473db94e
Bat3d2 <102492542+Bat3d2@users.noreply.github.com>
2022-11-25 00:06:41 -0700

    Version 3.2.7
    Delay vocales

--- Carrusel/app/src/main/java/com/example/carrusel/Vocales.kt
+++ Carrusel/app/src/main/java/com/example/carrusel/Vocales.kt
@@ -4,6 +4,7 @@ package com.example.carrusel
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ImageButton
 
 
@@ -19,31 +20,78 @@ class Vocales : AppCompatActivity() {
         val deciri = findViewById<ImageButton>(R.id.i)
         val deciro = findViewById<ImageButton>(R.id.o)
         val deciru = findViewById<ImageButton>(R.id.u)
+        val DURACION: Long = 500;
+        val CORTA: Long = 500;
 
 
         decira.setOnClickListener{
+            decira.isEnabled=false
+
             val mp = MediaPlayer.create(this, R.raw.letraa)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    decira.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
 
         decire.setOnClickListener{
+            decire.isEnabled=false
+
             val mp = MediaPlayer.create(this, R.raw.letrae)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    decire.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
 
         deciri.setOnClickListener{
+            deciri.isEnabled=false
+
             val mp = MediaPlayer.create(this, R.raw.letrai)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    deciri.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
 
         deciro.setOnClickListener{
+            deciro.isEnabled=false
+
             val mp = MediaPlayer.create(this, R.raw.letrao)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    deciro.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
 
         deciru.setOnClickListener{
+            deciru.isEnabled=false
+
             val mp= MediaPlayer.create(this, R.raw.letrau)
             mp.start()
+            Handler().postDelayed(Runnable {
+
+                Handler().postDelayed(Runnable {
+                    deciru.isEnabled=true
+                }, CORTA)
+            }, DURACION)
+
         }
 
 
